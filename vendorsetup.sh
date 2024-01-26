15e5813718cdd9adf20460d78fa13e5b930fa135
Joxquin <jotajoxquin@gmail.com>
2024-01-26 13:05:20 -0500

    lisa: Add Vendorsetup.sh

---
+++ vendorsetup.sh
@@ -0,0 +1,3 @@
+git clone https://github.com/crdroidandroid/android_vendor_xiaomi_lisa -b 14.0 vendor/xiaomi/lisa
+git clone https://github.com/Joxquin/android_kernel_xiaomi_lisa -b 14.0 kernel/xiaomi/lisa
+git clone https://github.com/PixelOS-Devices/vendor_xiaomi_camera-lisa -b fourteen vendor/xiaomi/camera
